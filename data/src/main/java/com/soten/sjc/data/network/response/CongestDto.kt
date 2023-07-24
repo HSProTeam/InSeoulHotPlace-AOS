ca3c1a9e5d0ad53913fe4323ce39f24536e975d4
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:30:52 +0900

    feat: CongestDto 구현

---
+++ data/src/main/java/com/soten/sjc/data/network/response/CongestDto.kt
@@ -0,0 +1,19 @@
+package com.soten.sjc.data.network.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class CongestDto(
+    @SerialName("area_nm")
+    val areaName: String,
+    @SerialName("congestion_color")
+    val congestionColor: String,
+    @SerialName("area_congest_lvl")
+    val areaCongestLevel: String,
+    @SerialName("area_congest_num")
+    val areaCongestNumber: Int,
+    val category: String,
+    val x: String,
+    val y: String,
+)
