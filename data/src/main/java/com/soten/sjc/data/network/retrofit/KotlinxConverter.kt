df60114ab6b68379b0bf241535b9cadc9995ad65
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:30:16 +0900

    feat: KotlinxConverter 구현

---
+++ data/src/main/java/com/soten/sjc/data/network/retrofit/KotlinxConverter.kt
@@ -0,0 +1,19 @@
+package com.soten.sjc.data.network.retrofit
+
+import com.soten.sjc.data.network.ApiResult
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+import okhttp3.ResponseBody
+import retrofit2.Converter
+
+internal class KotlinxConverter<T>(
+    private val deserializer: KSerializer<T>,
+    private val json: Json,
+) : Converter<ResponseBody, ApiResult<T>> {
+
+    override fun convert(value: ResponseBody): ApiResult<T> {
+        return value.use {
+            ApiResult.Success(json.decodeFromString(deserializer, it.string()))
+        }
+    }
+}
