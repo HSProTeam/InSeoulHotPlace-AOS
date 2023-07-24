88debcbba8e85e5adfeced3a7cb0e3a99130e5af
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:31:37 +0900

    feat: NetworkModule 구현

---
+++ data/src/main/java/com/soten/sjc/data/di/NetworkModule.kt
@@ -0,0 +1,22 @@
+package com.soten.sjc.data.di
+
+import com.soten.sjc.BuildConfig
+import com.soten.sjc.data.network.api.OpenApi
+import com.soten.sjc.data.network.retrofit.RetrofitFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesUpbitApi(): OpenApi {
+        return RetrofitFactory.create(BuildConfig.API_URL)
+            .create(OpenApi::class.java)
+    }
+}
