5255131437aaa62a34c6f5c1f741f23677b4d5d7
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:30:04 +0900

    feat: ApiResultCallAdapter 구현

---
+++ data/src/main/java/com/soten/sjc/data/network/retrofit/ApiResultCallAdapter.kt
@@ -0,0 +1,19 @@
+package com.soten.sjc.data.network.retrofit
+
+import com.soten.sjc.data.network.ApiResult
+import retrofit2.Call
+import retrofit2.CallAdapter
+import java.lang.reflect.Type
+
+internal class ApiResultCallAdapter<R : ApiResult<*>>(
+    private val responseType: Type,
+) : CallAdapter<R, ApiCall<R>> {
+
+    override fun responseType(): Type {
+        return responseType
+    }
+
+    override fun adapt(call: Call<R>): ApiCall<R> {
+        return ApiCall(call)
+    }
+}
