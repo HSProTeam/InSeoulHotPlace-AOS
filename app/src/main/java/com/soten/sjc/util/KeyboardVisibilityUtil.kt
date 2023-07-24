188f3ecf97e1676b98f824ce27b55905710695b6
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:32:20 +0900

    feat: KeyboardVisibilityUtil 구현

---
+++ app/src/main/java/com/soten/sjc/util/KeyboardVisibilityUtil.kt
@@ -0,0 +1,39 @@
+package com.soten.sjc.util
+
+import android.graphics.Rect
+import android.view.ViewTreeObserver
+import android.view.Window
+
+class KeyboardVisibilityUtil(
+    private val window: Window,
+    private val onShowKeyboard: (() -> Unit)? = null,
+    private val onHideKeyboard: (() -> Unit)? = null,
+) {
+
+    private val minKeyBoardHeightPx = 150
+
+    private val windowVisibleDisplayFrame = Rect()
+    private var lastVisibleDecorViewHeight: Int = 0
+
+    private val onGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+        window.decorView.getWindowVisibleDisplayFrame(windowVisibleDisplayFrame)
+        val visibleDecorViewHeight = windowVisibleDisplayFrame.height()
+
+        if (lastVisibleDecorViewHeight != 0) {
+            if (lastVisibleDecorViewHeight > visibleDecorViewHeight + minKeyBoardHeightPx) {
+                onShowKeyboard?.invoke()
+            } else if (lastVisibleDecorViewHeight + minKeyBoardHeightPx < visibleDecorViewHeight) {
+                onHideKeyboard?.invoke()
+            }
+        }
+        lastVisibleDecorViewHeight = visibleDecorViewHeight
+    }
+
+    fun init() {
+        window.decorView.viewTreeObserver.addOnGlobalLayoutListener(onGlobalLayoutListener)
+    }
+
+    fun detachKeyboardListeners() {
+        window.decorView.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalLayoutListener)
+    }
+}
