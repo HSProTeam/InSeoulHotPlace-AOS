d4861b04ef06017ab71609f5a6c37413123b2b4f
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:33:45 +0900

    feat: 메인 액티비티 기능 구현

---
+++ app/src/main/java/com/soten/sjc/ui/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.soten.sjc.ui
+
+import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
+import com.soten.sjc.R
+import com.soten.sjc.base.BaseActivity
+import com.soten.sjc.base.BaseAdapter
+import com.soten.sjc.databinding.ActivityMainBinding
+import com.soten.sjc.util.KeyboardVisibilityUtil
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
+    private val areaAdapter = BaseAdapter(R.layout.item_area)
+
+    private val keyboardVisibilityUtil by lazy {
+        KeyboardVisibilityUtil(window) {
+            binding.searchEditText.clearFocus()
+        }
+    }
+
+    override fun initViews() {
+        super.initViews()
+        binding.recyclerView.adapter = areaAdapter
+
+        keyboardVisibilityUtil.init()
+    }
+
+    override fun bindViews() {
+        super.bindViews()
+        binding.searchEditText.addTextChangedListener {
+            mainViewModel.inputKeyword(it.toString())
+        }
+    }
+
+    override fun observeData() {
+        super.observeData()
+
+        lifecycleScope.launch {
+            mainViewModel.searchCongestionInfos.collect{
+                areaAdapter.replaceAll(it)
+            }
+        }
+    }
+}
