598e5defaff8ef1b229379f21514a594863b15f8
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:31:15 +0900

    feat: CongestionInfo 구현

---
+++ domain/src/main/java/com/soten/sjc/domain/model/CongestionInfo.kt
@@ -0,0 +1,8 @@
+package com.soten.sjc.domain.model
+
+data class CongestionInfo(
+    val areaName: String,
+    val areaCongestLevel: String,
+    val areaCongestNumber: Int,
+    val category: String,
+)
