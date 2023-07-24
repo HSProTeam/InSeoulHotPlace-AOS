1ec92e5eab04fa6fc39e9e2c542b274cbae71189
ChoiYounho <maxosa78@gmail.com>
2023-07-24 18:32:55 +0900

    feat: BaseViewHolder 구현

---
+++ app/src/main/java/com/soten/sjc/base/BaseViewHolder.kt
@@ -0,0 +1,36 @@
+package com.soten.sjc.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.soten.sjc.BR
+
+open class BaseViewHolder(
+    private val binding: ViewDataBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    open fun <T> bind(item: T, clickedCallback: ItemClicked?) {
+        binding.setVariable(BR.item, item)
+
+        if (clickedCallback != null) {
+            binding.setVariable(BR.callback, clickedCallback)
+        }
+
+        binding.executePendingBindings()
+    }
+
+    companion object {
+        fun create(@LayoutRes layoutResId: Int, parent: ViewGroup) =
+            BaseViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    layoutResId,
+                    parent,
+                    false,
+                ),
+            )
+    }
+}
