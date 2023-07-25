17eb21cfa9a1ae67a71b6e98e2813a23d108cb07
ChoiYounho <maxosa78@gmail.com>
2023-07-25 18:46:29 +0900

    feat: repeatOnStart 확장함수 구현

---
+++ app/src/main/java/com/soten/sjc/extensions/LifeCycleExtension.kt
@@ -0,0 +1,15 @@
+package com.soten.sjc.extensions
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+
+fun LifecycleOwner.repeatOnStart(block: suspend () -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            block.invoke()
+        }
+    }
+}
