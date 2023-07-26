eee69a9b1f93be5b2125312821fa6263777988f1
ChoiYounho <maxosa78@gmail.com>
2023-07-26 15:35:35 +0900

    refactor: CongestionInfos 일급 컬렉션 코드 리팩토링 및 필터 적용

--- domain/src/main/java/com/soten/sjc/domain/model/congestion/CongestionInfos.kt
+++ domain/src/main/java/com/soten/sjc/domain/model/congestion/CongestionInfos.kt
@@ -1,13 +1,22 @@
 package com.soten.sjc.domain.model.congestion
 
+
 @JvmInline
 value class CongestionInfos(val value: List<CongestionInfo>) {
+
+    fun search(filter: CongestionFilter): CongestionInfos {
+        return CongestionInfos(
+            value.filter { congestionInfo ->
+                filter.isMatch(congestionInfo.areaName, congestionInfo.category)
+            }
+        )
+    }
 
     fun getCategories(): List<Category> {
         return value.groupBy { it.category }.keys.toList()
     }
 
-    fun getByCategory(category: Category): List<CongestionInfo> {
-        return value.filter { it.category == category }
+    companion object {
+        val EMPTY = CongestionInfos(emptyList())
     }
 }
