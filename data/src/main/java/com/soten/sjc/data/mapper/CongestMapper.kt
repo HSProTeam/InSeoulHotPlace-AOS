006dff10ba66acd9f642d3d19567610457fa0c26
ChoiYounho <maxosa78@gmail.com>
2023-08-09 15:00:40 +0900

    feat: 매퍼 기능 추가

--- data/src/main/java/com/soten/sjc/data/mapper/CongestMapper.kt
+++ data/src/main/java/com/soten/sjc/data/mapper/CongestMapper.kt
@@ -4,6 +4,12 @@ import com.soten.sjc.data.network.response.CongestDto
 import com.soten.sjc.domain.model.congestion.Category
 import com.soten.sjc.domain.model.congestion.CongestionInfo
 
-internal fun CongestDto.toDomain(): CongestionInfo {
-    return CongestionInfo(areaName, areaCongestLevel, areaCongestNumber, Category(category))
+internal fun CongestDto.toDomain(isBookmark: Boolean = false): CongestionInfo {
+    return CongestionInfo(
+        areaName,
+        areaCongestLevel,
+        areaCongestNumber,
+        Category(category),
+        isBookmark
+    )
 }
