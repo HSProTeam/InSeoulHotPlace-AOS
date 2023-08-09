a867534f74488bdbb27e56b6825410f1be2ec610
ChoiYounho <maxosa78@gmail.com>
2023-08-09 15:00:55 +0900

    feat: 레퍼지토리 기능 추가

--- domain/src/main/java/com/soten/sjc/domain/repository/CongestRepository.kt
+++ domain/src/main/java/com/soten/sjc/domain/repository/CongestRepository.kt
@@ -5,4 +5,8 @@ import com.soten.sjc.domain.model.congestion.CongestionInfos
 interface CongestRepository {
 
     suspend fun fetchCongests(): Result<CongestionInfos>
+
+    suspend fun insertBookmark(areaName: String)
+
+    suspend fun deleteBookmark(areaName: String)
 }
