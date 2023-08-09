5b9c25a0db7d6d53449357408b2d41e3c5455db2
Choiyounho <maxosa78@gmail.com>
2023-08-09 23:51:02 +0900

    feat: 클릭 기능 적용

--- app/src/main/java/com/soten/sjc/ui/MainActivity.kt
+++ app/src/main/java/com/soten/sjc/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.soten.sjc.ui
 
+import android.graphics.Color
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
@@ -9,17 +11,24 @@ import com.soten.sjc.R
 import com.soten.sjc.base.BaseActivity
 import com.soten.sjc.base.BaseAdapter
 import com.soten.sjc.databinding.ActivityMainBinding
+import com.soten.sjc.domain.model.congestion.CongestionInfo
 import com.soten.sjc.extensions.repeatOnStart
+import com.soten.sjc.extensions.setBackgroundTint
 import com.soten.sjc.util.KeyboardVisibilityUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.distinctUntilChanged
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), AreaClicked {
 
     private val mainViewModel by viewModels<MainViewModel>()
 
-    private val areaAdapter = BaseAdapter(R.layout.item_area)
+    private val areaAdapter = BaseAdapter(R.layout.item_area, this)
+
+    override fun onStart() {
+        super.onStart()
+        mainViewModel.fetchCongestionInfos()
+    }
 
     override fun initViews() {
         super.initViews()
@@ -43,6 +52,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         binding.deleteKeywordImage.setOnClickListener {
             binding.searchEditText.text.clear()
         }
+
+        binding.initChip.setOnClickListener {
+            mainViewModel.initSorter()
+        }
+
+        binding.nameChip.setOnClickListener {
+            mainViewModel.setSorter(isName = true)
+        }
+
+        binding.congestChip.setOnClickListener {
+            mainViewModel.setSorter(isLevel = true)
+        }
+
+        binding.sortingImage.setOnClickListener {
+            mainViewModel.setSorter()
+        }
     }
 
     override fun observeData() {
@@ -56,6 +81,37 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                     areaAdapter.replaceAll(it.value)
                 }
         }
+
+        repeatOnStart {
+            mainViewModel.congestionSorter.collect { sorter ->
+                setCheckingChip(binding.nameChip, sorter.isAreaName)
+                setCheckingChip(binding.congestChip, sorter.isLevel)
+
+                val sortingBackgroundResource = if (sorter.isAscend) {
+                    R.drawable.ic_sort_asc
+                } else {
+                    R.drawable.ic_sort_desc
+                }
+                binding.sortingImage.setBackgroundResource(sortingBackgroundResource)
+            }
+        }
+
+        repeatOnStart {
+            mainViewModel.congestionFilter.collect { filter ->
+                if (filter.category != null) {
+                    binding.categoryChip.text = filter.category?.value
+                    setCheckingChip(binding.categoryChip, true)
+                }
+
+                binding.categoryChip.text = "카테고리"
+                setCheckingChip(binding.categoryChip, false)
+                return@collect
+            }
+        }
+    }
+
+    override fun onClickArea(congestionInfo: CongestionInfo) {
+        mainViewModel.toggleBookmark(congestionInfo)
     }
 
     private fun initAdMob() {
@@ -71,10 +127,18 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private fun initKeyboardUtil() {
         val keyboardVisibilityUtil = KeyboardVisibilityUtil(window) {
-                binding.searchEditText.clearFocus()
-            }
+            binding.searchEditText.clearFocus()
+        }
 
         keyboardVisibilityUtil.init()
+    }
+
+    private fun setCheckingChip(chip: TextView, isSorting: Boolean) {
+        val tint = if (isSorting) R.color.darkGray else null
+        chip.setBackgroundTint(tint)
+
+        val textColor = if (isSorting) Color.WHITE else Color.BLACK
+        chip.setTextColor(textColor)
     }
 
     companion object {
