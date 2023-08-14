2bbd73c3fa3ad75ed5e2653a96337cdece7d5201
ChoiYounho <maxosa78@gmail.com>
2023-08-14 10:28:06 +0900

    feat: CongestSortState 적용

--- domain/src/main/java/com/soten/sjc/domain/model/congestion/CongestionInfos.kt
+++ domain/src/main/java/com/soten/sjc/domain/model/congestion/CongestionInfos.kt
@@ -4,62 +4,29 @@ package com.soten.sjc.domain.model.congestion
 value class CongestionInfos(val value: List<CongestionInfo>) {
 
     fun search(filter: CongestionFilter, sorter: CongestionsSorter): CongestionInfos {
-        return CongestionInfos(
-            sorting(
-                value.filter { congestionInfo ->
-                    filter.isMatch(congestionInfo.areaName, congestionInfo.category)
-                }.sortedBy { it.isBookmark },
-                sorter
-            )
-        )
+        return CongestionInfos(sorting(value.filter { congestionInfo ->
+            filter.isMatch(congestionInfo.areaName, congestionInfo.category)
+        }.sortedBy { it.isBookmark }, sorter))
     }
 
     fun toggleBookmark(areaName: String): CongestionInfos {
-        return CongestionInfos(
-            value.map { congestionInfo ->
-                if (congestionInfo.areaName == areaName) {
-                    congestionInfo.copy(isBookmark = congestionInfo.isBookmark.not())
-                } else {
-                    congestionInfo
-                }
+        return CongestionInfos(value.map { congestionInfo ->
+            if (congestionInfo.areaName == areaName) {
+                return@map congestionInfo.copy(isBookmark = congestionInfo.isBookmark.not())
             }
-        )
+
+            congestionInfo
+        })
     }
 
     fun getCategories(): List<Category> {
         return value.groupBy { it.category }.keys.toList()
     }
 
-    private fun sorting(list: List<CongestionInfo>, sorter: CongestionsSorter): List<CongestionInfo> {
-        val isAreaName = sorter.isAreaName
-        val isCongestNumber = sorter.isCongestNumber
-        val isAscend = sorter.isAscend
-        return when {
-            isAreaName && isAscend -> list.sortedWith(
-                compareByDescending<CongestionInfo> { it.isBookmark }
-                    .thenBy { it.areaName }
-            )
-
-            isAreaName && isAscend.not() -> list.sortedWith(
-                compareByDescending<CongestionInfo> { it.isBookmark }
-                    .thenByDescending { it.areaName }
-            )
-
-            isCongestNumber && isAscend -> list.sortedWith(
-                compareByDescending<CongestionInfo> { it.isBookmark }
-                    .thenBy { it.areaCongestNumber }
-            )
-
-            isCongestNumber && isAscend.not() -> list.sortedWith(
-                compareByDescending<CongestionInfo> { it.isBookmark }
-                    .thenByDescending { it.areaCongestNumber }
-            )
-
-            else -> list.sortedWith(
-                compareByDescending<CongestionInfo> { it.isBookmark }
-                    .thenBy { it.areaName }
-            )
-        }
+    private fun sorting(
+        congestions: List<CongestionInfo>, sorter: CongestionsSorter
+    ): List<CongestionInfo> {
+        return CongestSortState.of(sorter).sort(congestions)
     }
 
     companion object {
