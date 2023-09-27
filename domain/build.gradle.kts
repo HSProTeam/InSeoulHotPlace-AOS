40c8957c1ab2c5dfb76f58c47d8c37b4c83999ca
ChoiYounho <maxosa78@gmail.com>
2023-09-27 10:42:00 +0900

    docs: 도메인 모듈 Junit4 셋팅

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -10,10 +10,5 @@ java {
 dependencies {
     implementation(Kotlin.KOTLIN_STDLIB)
     testImplementation(Junit.JUNIT)
-    testImplementation(Junit.Jupiter.JUPITER)
     testImplementation(Google.TRUTH)
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
